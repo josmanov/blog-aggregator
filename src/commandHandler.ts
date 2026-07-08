cf6f25e6edde8da3097ec06d8aaab6ef796519f0
josmanov <jusuf.osmanovic01@gmail.com>
2026-07-08 18:40:38 +0300

    create registry & register login command

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -5,21 +5,20 @@ export type CommandsRegistry = {
     [key: string]: CommandHandler;
 }
 
-function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler) {
+export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler) {
     registry[cmdName] = handler;
 }
 
-function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
+export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
     if (registry[cmdName]) {
         registry[cmdName](cmdName, ...args);
     }
 }
 
-function handlerLogin(cmdName: string, ...args: string[]) {
+export function handlerLogin(cmdName: string, ...args: string[]) {
     if (args.length === 0) {
         throw new Error("the login handler expects a single argument, the username");
     }
-    console.log(`Should be the name: ${args[0]}`);
     setUser(args[0]);
     console.log("the user has been set");
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { setUser,  readConfig } from "./config.js"
+import { CommandsRegistry, registerCommand, handlerLogin, runCommand } from "./commandHandler.js"
 
 function main() {
-  setUser("Lane");
+  const registry: CommandsRegistry = {};
+  registerCommand(registry, "login", handlerLogin);
+  const validArgs = process.argv.slice(2);
+
+  if (validArgs.length < 1) {
+    console.log("no arguments provided");
+    process.exit(1);
+  }
+  const command = validArgs[0];
+  const restCommands = validArgs.slice(1);
+  try {
+    runCommand(registry, command, ...restCommands)
+  } catch(error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+      process.exit(1);
+    }
+  }
+  /*
+
   const config = readConfig();
   console.log(config);
+
+  */
 }
 
 main();
