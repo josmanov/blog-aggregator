f4a79f6aa39712f7d93b5cfc0432369b636d6046
josmanov <jusuf.osmanovic01@gmail.com>
2026-07-18 04:05:53 +0300

    add posts table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -42,3 +42,19 @@ export const feed_follows = pgTable("feed_follows", {
     unique().on(table.userId, table.feedId),
 ],
 );
+
+export const posts = pgTable("posts", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  title: text("title"),
+  url: text("url").notNull().unique(),
+  description: text("description"),
+  publishedAt: timestamp("published_at"),
+  feedId: uuid("feed_id")
+    .notNull()
+    .references(() => feeds.id, { onDelete: "cascade" }),
+});
